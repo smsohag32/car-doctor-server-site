28db4d7fe3d761fc1267044c148e08e1f2ddc073
SM Sohag <sohagsheik32@gmail.com>
2023-05-14 22:23:06 +0600

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# query params
+
+- diay some data read kora jay ...
+
+- query params bookings?email=sohag@gmail.com
+- find er parameter er vitor query ta set korle query ta matching kore data dibay ...
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
@@ -37,13 +37,93 @@ async function run() {
     await client.connect();
 
     const servicesCollection = client.db('carDoctor').collection('services');
-
-    app.get('/services', async(req,res)=> {
-        const cursor = servicesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+    const bookingCollection = client.db('carDoctor').collection('bookings')
+    // app.get('/services', async(req,res)=> {
+    //     const cursor = servicesCollection.find();
+    //     const result = await cursor.toArray();
+    //     res.send(result)
+    // })
+// all data get 
+    app.get('/services', async (req,res)=>{
+      const result = await servicesCollection.find().toArray();
+      res.send(result);
     })
 
+
+    // get specific id data in mongodb
+
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const option = {
+        projection: {
+          _id: 1 ,
+          title: 1,
+          img: 1,
+          price: 1,
+        }
+      }
+      const result = await servicesCollection.findOne(query, option);
+      res.send(result)
+    })
+    // query params  ?email=sohag@gmail.com
+    app.get('/bookings', async(req,res)=>{
+      // console.log(req.query);
+      // let query = {};
+      // if(req.query?.email){
+      //   query = {email: req.query.email}
+      // }
+      // const result = await bookingCollection.find(query).toArray();
+      // res.send(result)
+      let query = {}
+      if(req.query.email){
+        query = { email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+
+    })
+
+    // post bookings
+    app.post('/bookings', async(req,res)=>{
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+     
+    })
+    // patch 
+    app.patch('/bookings/:id', async(req, res) => {
+      // const id = req.params.id;
+      // const filter = {_id: new ObjectId(id)}
+      // const updateBooking = req.body;
+      // const updateDoc = {
+      //   $set: {
+      //     status: updateBooking.status,
+      //   }
+      // }
+      // const result = await bookingCollection.updateOne(filter, updateDoc);
+      // res.send(result)
+
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        }
+      }
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // delete
+
+    app.delete('/bookings/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
